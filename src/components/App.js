0d3b41ac92dc7d6910079ff456369d2728b7cf69
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2022-12-11 20:58:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React, {components, useState} from "react";
-import '../styles/App.css';
+import React, {useState,useEffect} from "react";
+import'../styles/App.css';
 
-  const App = ({slides}) => {
+  const App = ({slides}) =>{
   const[slideIndex,setSlideIndex]=useState(0);
   const[prevFlag,setPrevFlag]=useState(true);
   const[nextFlag,setNextFlag]=useState(false);
   const[restartFlag,setRestartFlag]=useState(true);
-  useEffect(() => {
+  useEffect(() =>{
     //if current slide is the first one
     if(slideIndex === 0){
       setPrevFlag(true);
       setRestartFlag(true);
-    }else{
+    } else {
       setPrevFlag(false);
       setRestartFlag(false);
     }
     //if current slide is the last one
-    if(slideIndex===slides.length-1) {
+    if(slideIndex === slides.length - 1){
       setNextFlag(true);
-    }else{
+    } else {
       setNextFlag(false);
     }
   },[slideIndex])
-  const handlePrevClick = () => {
+  const handlePrevClick = () =>{
     setSlideIndex((prevState)=>prevState - 1);
   }
-  const handleNextClick = () => {
+  const handleNextClick = () =>{
     setSlideIndex((prevState)=>prevState + 1);
   }
-  const handleRestartClick = () => {
+  const handleRestartClick = () =>{
     setSlideIndex(0);
   }
   return (
@@ -36,12 +36,12 @@ import '../styles/App.css';
     <h1 id="heading">Amazon Prime: Trending</h1>
     <div>
     <div>
-    <h1 data- testid="title'>{slides[slideIndex].title}</h1>
-    <p data- testid="text">{slides[slideIndex].text}</p>
+    <h1 data-testid="title'>{slides[slideIndex].title}</h1>
+    <p data-testid="text">{slides[slideIndex].text}</p>
     </div>
-    <button data-testid="button-prev"onclick={handlePrevClick}disabled={prevFlag}>Prev</button>
-    <button data-testid="button-next"onclick={handleNextClick}disabled={nextFlag}>Next</button>
-    <button data-testid="button-restart"onclick={handleRestartClick}disabled={restartFlag}>Restart</button>
+    <button data-testid="button-prev"onClick={handlePrevClick}disabled={prevFlag}>Prev</button>
+    <button data-testid="button-next"onClick={handleNextClick}disabled={nextFlag}>Next</button>
+    <button data-testid="button-restart"onClick={handleRestartClick}disabled={restartFlag}>Restart</button>
     </div>
     </>
   )
